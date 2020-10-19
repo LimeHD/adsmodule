4211ecc3a4c66a32302ff5645d92a00592cc507f
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-19 15:35:12 +0300

    Add AdTypeIdentity sealed class

---
+++ adsmodule/src/main/java/tv/limehd/adsmodule/AdTypeIdentity.kt
@@ -0,0 +1,11 @@
+package tv.limehd.adsmodule
+
+sealed class AdTypeIdentity(val typeIdentity: String) {
+
+    object Google : AdTypeIdentity("googleinterstitial")
+    object MyTarget : AdTypeIdentity("MyTarget")
+    object VideoNow : AdTypeIdentity("videonow")
+    object Hyperaudience : AdTypeIdentity("hyperaudience")
+    object Adriver : AdTypeIdentity("adriver")
+
+}
